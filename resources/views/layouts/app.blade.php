4fce2ca4ad2289afe319028e9ad577f889a445a9
Gustavo Ponce <poncegerardo.p@gmail.com>
2020-08-15 21:04:42 -0500

    arreglos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,7 +57,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}">
+                                    <a class="dropdown-item" href="#">
                                         Configuración
                                     </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
